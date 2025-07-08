ba789aecee452d07ac7bb5474a2e29cff98ad063
portoinfo <portoinfo@sercomtel.com.br>
2025-07-08 12:15:59 -0300

    First commit

---
+++ usando_captcha_6.py
@@ -0,0 +1,7 @@
+# pegar uma imagem de captcha
+from resolver_captcha_4 import quebrar_captcha
+
+texto_captcha = quebrar_captcha()
+
+print(f"Meu texto do CAPTCHA foi: {texto_captcha}")
+# resolver essa imagem de captcha
